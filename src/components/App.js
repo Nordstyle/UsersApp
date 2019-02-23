b76ecfd20d7f9d92747dc2259353b9b3893c2dc1
AnatoliyZ <zolotukhinwebpr@mail.ru>
2019-02-23 15:23:46 +0400

    styles for main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from "react"
 import { Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 import sf from './pages/stackoverflow'
 import gh from './pages/github'
 
 import '../styles/App.css'
 
+const Wrapper = styled.div`
+    max-width: 120rem;
+    margin: 0 auto;
+    padding-left: 0.6rem;
+    padding-right: 0.6rem;
+`;
 
+const Header = styled.nav`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem 0;
+    box-shadow: 0 10px 20px -10px rgba(0, 0, 0, .09), 0 5px 10px -5px rgba(0, 0, 0, .09);
+    border-bottom: 0.1rem solid #e2e2e2;  
+    margin-bottom: 2rem;
+`;
+
+const LinkContainer = styled.li`
+    position: relative;
+    padding: 1rem 2.4rem;
+    margin: 0 1rem;
+    font-family: 'Pacifico', cursive;
+    font-size: 2rem;
+    color: #FFF;
+    text-decoration: none;	
+    list-style-type: none;
+`;
+
+const RouteLink = styled(Link)`
+    color: #24292e;
+    transition: 0.3s;
+    &:hover {
+        text-shadow: 0px -1px #F48024;
+    }
+`;
 class App extends Component {
     render() {
         return (
-            <div>
-                <header>
-                    <Link to="/">StackOverflow</Link>
-                    <Link to="/gh">Github</Link>
-                </header>
+            <Wrapper>
+                <Header>
+                    <LinkContainer>
+                        <RouteLink to="/">StackOverflow</RouteLink>
+                    </LinkContainer>
+                    <LinkContainer>
+                        <RouteLink to="/gh">Github</RouteLink>
+                    </LinkContainer>
+                </Header>
 
                 <main>
                     <Route exact path="/" component={sf} />
                     <Route path="/gh" component={gh} />
                 </main>
-            </div>
+            </Wrapper>
         );
     }
 }
