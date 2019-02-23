78f5cd26bd1321874ee272df8c42a9533a5b1f72
AnatoliyZ <zolotukhinwebpr@mail.ru>
2019-02-23 14:23:55 +0400

    pages

---
+++ src/components/pages/github/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const gh = () => (
+  <div>
+    <h1>github</h1>
+    <p>hi, are u from gh?</p>
+  </div>
+)
+
+export default gh
---
+++ src/components/pages/stackoverflow/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const sf = () => (
+  <div>
+    <h1>stack overflow</h1>
+    <p>hi, are u from sf?</p>
+  </div>
+)
+
+export default sf
