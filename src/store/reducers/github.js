5d8f90affd3954d7a1f717d916f1facae4b8e668
AnatoliyZ <zolotukhinwebpr@mail.ru>
2019-02-24 14:05:13 +0400

    fix error

--- src/store/reducers/github.js
+++ src/store/reducers/github.js
@@ -2,8 +2,8 @@ import { FETCH_GITHUB_REPOS, FETCHING_GITHUB_REPOS, GITHUB_ERROR } from './../co
 
 export const usersGithubRepos = (state = { isLoading: false, isError: false, data: [] }, action) => {
   switch (action.type) {
-    case FETCHING_GITHUB_REPOS: return ({ isLoading: true, isError: false, data: [] });
     case FETCH_GITHUB_REPOS: return ({ isLoading: false, isError: false, data: action.payload });
+    case FETCHING_GITHUB_REPOS: return ({ isLoading: true, isError: false, data: [] });
     case GITHUB_ERROR: return ({ isLoading: false, isError: true, data: action.payload })
     default: return state;
   }
--- src/store/reducers/stackoverflow.js
+++ src/store/reducers/stackoverflow.js
@@ -3,7 +3,7 @@ import { FETCH_OVERFLOW_USERDATA, FETCHING_OVERFLOW_USERDATA, OVERFLOW_ERROR } f
 export const usersOverflowData = (state = { isLoading: false, isError: false, data: [] }, action) => {
   switch (action.type) {
     case FETCH_OVERFLOW_USERDATA: return ({ isLoading: false, isError: false, data: action.payload });
-    case FETCHING_OVERFLOW_USERDATA: return ({ isLoading: true, isError: false, data: [...state.data] });
+    case FETCHING_OVERFLOW_USERDATA: return ({ isLoading: true, isError: false, data: [] });
     case OVERFLOW_ERROR: return ({ isLoading: false, isError: true, data: action.payload})
     default: return state;
   }
